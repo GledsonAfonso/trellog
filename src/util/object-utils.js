7dfb16668af0feec4f64d7d031e2fa3a00b312f3
Gledson A <g_a6@live.com>
2020-10-03 19:42:45 -0300

    Adding method in object utils to remove blank properties from obj

--- src/util/object-utils.js
+++ src/util/object-utils.js
@@ -1,3 +1,15 @@
 const isBlank = (object) => object && object !== null ? Object.keys(object).length === 0 : true;
 
-module.exports = { isBlank };
+const removeEmptyPropertiesFrom = (object) => {
+    Object.keys(object).forEach(key => {
+        if (!!object[key] && typeof object[key] === 'object') {
+            removeEmptyPropertiesFrom(object[key]);
+        }
+
+        if (!object[key] || (typeof object[key] === 'object' && isBlank(object[key]))) {
+            delete object[key];
+        }
+    });
+};
+
+module.exports = { isBlank, removeEmptyPropertiesFrom };
--- test/util/object-utils.test.js
+++ test/util/object-utils.test.js
@@ -1,4 +1,4 @@
-const { isBlank } = require('../../src/util/object-utils')
+const { isBlank, removeEmptyPropertiesFrom } = require('../../src/util/object-utils')
 
 describe('object utils', () => {
     test('should be able to verify if object is blank', () => {
@@ -11,5 +11,44 @@ describe('object utils', () => {
         expect(isBlank(nullObject)).toBeTruthy();
         expect(isBlank(blankObject)).toBeTruthy();
         expect(isBlank(validObject)).toBeFalsy();
+    });
+
+    test('should be able to remove empty properties from a given object', () => {
+        let object = {
+            a: undefined,
+            b: null,
+            c: '',
+            d: 0,
+            e: {},
+            f: 'this should not be deleted',
+            g: {
+                h: undefined,
+                i: null,
+                j: '',
+                k: 0,
+                l: {},
+                m: 'this too should not be deleted'
+            },
+            n: {
+                o: undefined,
+                p: null,
+                q: '',
+                r: 0,
+                s: {}
+            },
+            t: 123
+        };
+
+        const expectedObject = {
+            f: 'this should not be deleted',
+            g: {
+                m: 'this too should not be deleted'
+            },
+            t: 123
+        };
+
+        removeEmptyPropertiesFrom(object);
+
+        expect(object).toEqual(expectedObject);
     });
 });
